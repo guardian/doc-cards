76d114326ee314ddd0862acd79dc655722c95f7f
Duarte Carrilho da Graça <duarte.carrilho.freelance@theguardian.com>
2017-02-07 17:06:28 +0000

    prepare to build more things

--- scripts/compile.js
+++ scripts/compile.js
@@ -4,54 +4,41 @@ var fs = require('fs');
 var mkpath = require('mkpath');
 var ncp = require('ncp').ncp;
 
-// templates
-var genericJson = fs.readFileSync('src/templates/html.json', 'utf8');
-var thrasherHtml = fs.readFileSync('src/templates/thrasher.html', 'utf8');
-var thrasherCss = fs.readFileSync('build/css/thrasher.css', 'utf8');
-var snapHtml = fs.readFileSync('src/templates/snap.html', 'utf8');
-var snapCss = fs.readFileSync('build/css/snap.css', 'utf8');
-var homeHtml = fs.readFileSync('src/templates/index.html', 'utf8');
-
-// data
+// Define what we're building
+var snapTemplates = ['thrasher', 'snap'];
 var documentaries = require('../src/documentaries.json');
 
-for (var i = 0; i < documentaries.length; i++) {
-    generateThrasher(documentaries[i]);
-    generateSnap(documentaries[i]);
-}
-
+// Build
 generateHomePage();
 copyPosters();
-
-function generateThrasher(data) {
-    var htmlTemplate = handlebars.compile(thrasherHtml);
-        data.css = thrasherCss.replace(/handle/g, data.handle);
-        data.html = JSON.stringify(htmlTemplate(data), null);
-
-    var jsonTemplate = handlebars.compile(genericJson);
-    var json = jsonTemplate(data);
-
-    var path = 'build/thrasher/' + data.handle;
-    mkpath(path, function() {
-        fs.writeFile(path + '/source.json', json);
-    });
+for (var i = 0; i < snapTemplates.length; i++) {
+    for (var j = 0; j < documentaries.length; j++) {
+        generate(snapTemplates[i], documentaries[j]);
+    }
 }
 
-function generateSnap(data) {
-    var htmlTemplate = handlebars.compile(snapHtml);
-        data.css = snapCss.replace(/handle/g, data.handle);
+function generate(template, data) {
+
+    var jsonFile = fs.readFileSync('src/templates/html.json', 'utf8');
+    var htmlFile = fs.readFileSync('src/templates/'+template+'.html', 'utf8');
+    var cssFile = fs.readFileSync('build/css/'+template+'.css', 'utf8');
+
+
+    var htmlTemplate = handlebars.compile(htmlFile);
+        data.css = cssFile.replace(/handle/g, data.handle);
         data.html = JSON.stringify(htmlTemplate(data), null);
 
-    var jsonTemplate = handlebars.compile(genericJson);
+    var jsonTemplate = handlebars.compile(jsonFile);
     var json = jsonTemplate(data);
 
-    var path = 'build/snap/' + data.handle;
+    var path = 'build/'+template+'/' + data.handle;
     mkpath(path, function() {
         fs.writeFile(path + '/source.json', json);
     });
 }
 
 function generateHomePage() {
+    var homeHtml = fs.readFileSync('src/templates/index.html', 'utf8');
     var htmlTemplate = handlebars.compile(homeHtml);
 
     fs.writeFile('build/index.html', htmlTemplate(documentaries));
